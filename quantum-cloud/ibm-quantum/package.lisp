15694f82bea72d2e61fa67042db0b7aac607d865
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2022-03-09 07:30:50 -0500

    add skeleton files for QUANTUM-CLOUD/IBM-QUANTUM package

---
+++ quantum-cloud/ibm-quantum/package.lisp
@@ -0,0 +1,9 @@
+;; Copyright (c) 2022, "the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
+;; Released under the MIT License. See open-blackfire/LICENSE for more information.
+
+(in-package :cl-user)
+
+(defpackage open-blackfire/quantum-cloud/ibm-quantum
+  (:nicknames obfq/ibm)
+  (:use cl protocol-phi)
+  (:export))
