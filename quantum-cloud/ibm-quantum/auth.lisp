63a4cc2ebc8955360d137c47860d085a29289698
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2022-03-12 08:18:17 -0500

    update CLEAR-AUTHORIZATION-CACHE to use inline URI-TEMPLATE expansion, and note-to-self to return an ERROR-CONTAINER response object from a DEX:HTTP-REQUEST-FAILED response

--- quantum-cloud/ibm-quantum/auth.lisp
+++ quantum-cloud/ibm-quantum/auth.lisp
@@ -4,9 +4,10 @@
 (in-package :open-blackfire/quantum-cloud/ibm-quantum)
 
 (defmethod clear-authorization-cache ()
-  "Clear the authorization cache for the specified user.  This is useful if a user's role changes."
-  (handler-case (dex:post (ibmq-route "/authorize"))
+  "Clear the authorization cache for the specified user. This is useful if a user's role changes."
+  (handler-case (dex:post #U{*ibmq-server*}/authorize)
     ;; 204 Cache successfully deleted
     ;; 500 Internal error
     (dex:http-request-failed (e)
+      ;; also return an ERROR-CONTAINER response object as specified by the OpenAPI definition
       (format *error-output* "The server at ~S returned ~D" *ibmq-server* (dex:response-status e)))))
