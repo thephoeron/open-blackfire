1f54ee4ddc25cdaeb9220c345822a23568a04e56
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2022-03-16 07:54:11 -0400

    add response-handling logic to IBM Quantum Devices and Jobs REST API methods, with some minor commentary to explain what's going on when it's not obvious

--- quantum-cloud/ibm-quantum/devices.lisp
+++ quantum-cloud/ibm-quantum/devices.lisp
@@ -5,20 +5,40 @@
 
 (defmethod list-devices ()
   "Returns a list of all the devices your service instance has access to."
-  (dex:get #U{*ibmq-server*}/devices :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/devices :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'devices-response))
+      (otherwise nil))))
 
 (defmethod get-device-properties (device-id)
   "Returns the properties for the specified device."
-  (dex:get #U{*ibmq-server*}/devices/{device-id}/properties :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/devices/{device-id}/properties :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'device-properties-response))
+      (otherwise nil))))
 
 (defmethod get-device-configuration (device-id)
   "Returns the configuration for the specified device."
-  (dex:get #U{*ibmq-server*}/devices/{device-id}/configuration :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/devices/{device-id}/configuration :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'device-configuration-response))
+      (otherwise nil))))
 
 (defmethod get-device-status (device-id)
   "Returns the status for the specified device ID."
-  (dex:get #U{*ibmq-server*}/devices/{device-id}/status :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/devices/{device-id}/status :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'device-status-response))
+      (otherwise nil))))
 
 (defmethod get-device-defaults (device-id)
   "Returns the defaults for the specified device."
-  (dex:get #U{*ibmq-server*}/devices/{device-id}/defaults :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/devices/{device-id}/defaults :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'device-defaults-response))
+      (otherwise nil))))
--- quantum-cloud/ibm-quantum/jobs.lisp
+++ quantum-cloud/ibm-quantum/jobs.lisp
@@ -5,36 +5,109 @@
 
 (defmethod list-jobs ()
   "List the quantum program jobs you have run."
-  (dex:get #U{*ibmq-server*}/jobs :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/jobs :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'jobs-response))
+      (400 (json-to-clos body 'error-container))
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod create-job ()
   "Run a quantum program. Note the returned job ID.  You will use it to check the job's status and review results."
-  (dex:post #U{*ibmq-server*}/jobs :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:post #U{*ibmq-server*}/jobs :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-close body 'job-create-response))
+      (400 (json-to-clos body 'error-container))
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod get-job-details (job-id)
   "List the details about the specified quantum program job."
-  (dex:get #U{*ibmq-server*}/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 (json-to-clos body 'job-response))
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod delete-job (job-id)
   "Delete the specified job."
-  (dex:delete #U{*ibmq-server*}/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:delete #U{*ibmq-server*}/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (204 t)
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod get-job-results (job-id)
   "Return the final result from this job."
-  (dex:get #U{*ibmq-server*}/jobs/{job-id}/results :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/jobs/{job-id}/results :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 body)
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod get-job-interim-results (job-id)
   "Return the interim results from this job. Interim results are kept two days after the job has finished running."
-  (dex:get #U{*ibmq-server*}/jobs/{job-id}/interim-results :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/jobs/{job-id}/interim-results :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 body)
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod get-job-logs (job-id)
   "List all job logs for the specified job."
-  (dex:get #U{*ibmq-server*}/jobs/{job-id}/logs :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/jobs/{job-id}/logs :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (200 body)
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      (410 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod cancel-job (job-id)
   "Cancels the specified job."
-  (dex:post #U{*ibmq-server*}/jobs/{job-id}/cancel :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:post #U{*ibmq-server*}/jobs/{job-id}/cancel :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      (204 t)
+      (401 (json-to-clos body 'error-container))
+      (403 (json-to-clos body 'error-container))
+      (404 (json-to-clos body 'error-container))
+      ;; Job is a non-cancellable status
+      (409 (json-to-clos body 'error-container))
+      (otherwise nil))))
 
 (defmethod get-job-results-stream (job-id)
   "Get a result stream as the job runs using WebSocket."
-  (dex:get #U{*ibmq-server*}/stream/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*))))
+  (multiple-value-bind (body status response-headers uri stream)
+      (dex:get #U{*ibmq-server*}/stream/jobs/{job-id} :headers (list (cons "x-access-token" *ibmq-api-key*)))
+    (case status
+      ;; this should be a WebSocket stream
+      (200 body)
+      ;; Found, also returns a WebSocket stream
+      (303 (json-to-clos body 'error-container))
+      ;; Unauthorized
+      (401 (json-to-clos body 'error-container))
+      ;; Internal server error
+      (500 (json-to-clos body 'error-container))
+      ;; In case of total failure, return NIL
+      (otherwise nil))))
