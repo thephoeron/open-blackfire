b0e3aed12b6b6b87b30ac5301083081df48c8f12
the Phoeron" Colin J.E. Lupton <thephoeron@protonmail.com>
2022-03-18 10:04:00 -0400

    add skeleton objects for v2 of IBM Quantum REST API

--- quantum-cloud/ibm-quantum/objects.lisp
+++ quantum-cloud/ibm-quantum/objects.lisp
@@ -2,3 +2,214 @@
 ;; Released under the MIT License. See open-blackfire/LICENSE for more information.
 
 (in-package :open-blackfire/quantum-cloud/ibm-quantum)
+
+(defclass check ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass health-response ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hub ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass backend ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass object-storage-info ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hub-info-hub ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hub-info-group ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hub-info-project ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hub-info ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass run-job-response ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass job-input ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass object-storage-url ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass job ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass job-data-uploaded-response ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass info-queue ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass job-status ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass parameter ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass device-props-gate ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass qubit ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass device-properties ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass job-update ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass gate ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass hamiltonian ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass device ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass cmd-def ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass discriminator ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass meas-kernel ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass pulse ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass device-defaults ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass jobs-limit ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass backend-status ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass booking-hub-info-hub ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass booking-hub-info-group ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass booking-hub-info-project ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass booking-hub-info ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass booking ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass date ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass access-token ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass api-token ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass urls ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass user ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
+(defclass version-response ()
+  ()
+  (:metaclass json-serializable-class)
+  (:documentation ""))
+
